30452a723d429ee462763e4902cff82ff3c3f56e
efleitasalva <ejfleitas@gmail.com>
2024-11-27 22:00:06 -0300

    ultimo core agregado

--- machine_learning/ejercicios_core/utils.py
+++ machine_learning/ejercicios_core/utils.py
@@ -10,34 +10,10 @@ def cargar_datos(ruta_archivo):
 def exploracion_inicial(df):
     print("Informacion del dataset")
     print(df.info())
-    print("Estadisticas del dataset")
-    print(df.describe())    
-    # Calcular medidas de tendencia central y dispersión para cada variable numérica
-    print("Medidas de tendencia central y dispersión para cada variable numérica")
-    measures = {}
-    numeric_columns = df.select_dtypes(include=[float, int]).columns
-    ##Si los datos numericos estan en el dataframe
-    if numeric_columns.size > 0:
-        for column in numeric_columns:
-            measures[column] = {
-                'Mean': df[column].mean(),
-                'Median': df[column].median(),
-                'Mode': df[column].mode()[0],
-                'Std Dev': df[column].std(),
-                'Range': df[column].max() - df[column].min()
-            }
-    else:
-        print("##No se encontraron columnas numéricas en el DataFrame.##")  
-
-    # Mostrar medidas de tendencia central y dispersión
-    for column, stats in measures.items():
-        print(f"\n{column}:")
-        for measure, value in stats.items():
-            print(f"  {measure}: {value}")
-    # Imprimir cantidad de filas y columnas del dataset
-    print("Cantidad de filas: ", df.shape[0])
-    print("Cantidad de columnas: ", df.shape[1])
-
+    print(df.head())
+    ##Imprime ultimas 5 filas
+    print("nÚltimas 5 filas del DataFrame:")
+    print(df.tail(5))
 # Identificar valores faltantes
 def identificar_valores_faltantes(df):
     print("Valores faltantes en el dataset")
@@ -70,7 +46,35 @@ def modificar_valores_categoricos(df):
     for column in df.select_dtypes(include=['object']).columns:
         df[column] = df[column].str.upper().str.strip()
     return df
+###Estadisiticas descriptivas
+def estadisticas_descriptivas(df):
+    print("Estadisticas del dataset")
+    print(df.describe())    
+    # Calcular medidas de tendencia central y dispersión para cada variable numérica
+    print("Medidas de tendencia central y dispersión para cada variable numérica")
+    measures = {}
+    numeric_columns = df.select_dtypes(include=[float, int]).columns
+    ##Si los datos numericos estan en el dataframe
+    if numeric_columns.size > 0:
+        for column in numeric_columns:
+            measures[column] = {
+                'Mean': df[column].mean(),
+                'Median': df[column].median(),
+                'Mode': df[column].mode()[0],
+                'Std Dev': df[column].std(),
+                'Range': df[column].max() - df[column].min()
+            }
+    else:
+        print("##No se encontraron columnas numéricas en el DataFrame.##")  
 
+    # Mostrar medidas de tendencia central y dispersión
+    for column, stats in measures.items():
+        print(f"\n{column}:")
+        for measure, value in stats.items():
+            print(f"  {measure}: {value}")
+    # Imprimir cantidad de filas y columnas del dataset
+    print("Cantidad de filas: ", df.shape[0])
+    print("Cantidad de columnas: ", df.shape[1])
 ### Verificacion de outliers
 def plot_grafico_outliers(num_features,nrows, ncols, df):
     # Ajustar dinámicamente el tamaño de la figura
@@ -81,6 +85,24 @@ def plot_grafico_outliers(num_features,nrows, ncols, df):
     for i, feature in enumerate(num_features):
         sns.boxplot(x=df[feature], ax=axes[i], color="#75f8f2")
         axes[i].set_title(feature)
+
+    # Ocultar ejes sobrantes si hay más subplots de los necesarios
+    for j in range(i + 1, len(axes)):
+        axes[j].set_visible(False)
+
+    plt.tight_layout()
+    plt.show()
+
+### Plot univariados de variables categoricas# Ajustar dinámicamente el tamaño de la figura
+def plot_univariados_categoricos(df,ncols,nrows):
+    figsize = (ncols * 5, nrows * 5)  # 5 unidades de ancho y alto por subplot
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    axes = axes.flatten()  # Aplanar para facilitar el acceso
+
+    # Plotear variables categóricas
+    for i, column in enumerate(df.select_dtypes(include=['object', 'category']).columns):
+        sns.countplot(data=df, x=column, ax=axes[i])
+        axes[i].set_title(column)
 
     # Ocultar ejes sobrantes si hay más subplots de los necesarios
     for j in range(i + 1, len(axes)):
